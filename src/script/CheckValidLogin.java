8b8b0cc970544ad170dcb52576c72e3143a57836
manjunath Sastry kypa <manjukypa@gmail.com>
2018-03-22 16:32:31 +0530

    update

--- src/script/CheckValidLogin.java
+++ src/script/CheckValidLogin.java
@@ -12,12 +12,16 @@ public class CheckValidLogin extends BaseTest{
 	public void testValidLogin() {
 		String un=Excel.getValue(XL_PATH,"ValidLogin",1,0);
 		String pw=Excel.getValue(XL_PATH, "ValidLogin",1,1);
+//		String un = "manjukypa";
+//		String pw = "bollywood";
 		String eTitle=Excel.getValue(XL_PATH,"ValidLogin",1,2);
 		LoginPage l=new LoginPage(driver);
 		//Enter Valid UN
 		l.setUserName(un);
+		System.out.println(un);
 		//Enter Valid pw
 		l.setPassword(pw);
+		System.out.println(pw);
 		//Click login
 		l.clickLogin();
 		//verify home page....
